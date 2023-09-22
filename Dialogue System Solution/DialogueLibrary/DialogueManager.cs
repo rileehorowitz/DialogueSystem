eecdf1552b92428f3f97a7754b327f1d487241c2
Rilee Horowitz <rileehorowitz@gmail.com>
2023-09-22 10:18:50 -0400

    Created static class DialoguePrinter to handle writing dialogue to the console

--- Dialogue System Solution/DialogueLibrary/DialogueManager.cs
+++ Dialogue System Solution/DialogueLibrary/DialogueManager.cs
@@ -11,17 +11,11 @@ namespace DialogueLibrary
         private Scene? currentScene;
         private DialogueNode? currentNode;
 
-        public static bool isWriting;
-
-        public DialogueManager()
-        {
-
-        }
+        public DialogueManager() { }
         public DialogueManager(Scene scene) => StartScene(scene);
 
         public void StartScene(Scene scene)
         {
-
             if (currentScene == null)
             {
                 currentScene = scene;
@@ -69,70 +63,19 @@ namespace DialogueLibrary
                 EndScene();
             }
         }
-
-        private string ReadNode(DialogueNode node)
+        private void ReadNode(DialogueNode node)
         {
             if (node != null)
             {
-                string output = WriteText(node);
-                //if input is good
-                MoveToNode(node.NextNode);
-                return output;
+                DialoguePrinter.PrintText(node, true);
             }
             else //woah how'd we get here? The node we're reading is null, so we should return an empty string. If we're reading a null node, we should probably end the scene
             {
                 EndScene();
-                return "";
+                return;
             }
-        }
-
-        private string WriteText(DialogueNode node)
-        {
-            isWriting = true;
-            int writeDelay = 50;
-            Console.WriteLine(node.Text.TextSpeaker);
-            string nodeText = $"{node}";
-            string displayText = "";
-
-            //write the display text to the console one character at a time
-            for(int i = 0; i < nodeText.Length; i++)
-            {
-                displayText = nodeText.Substring(0, i + 1);
-
-                //store what line the dialogue starts on
-                int startLine = Console.CursorTop;
-
-                //print the current displayText
-                Console.Write($"{displayText}");
-
-                //store what line the dialogue ends on
-                int endLine = Console.CursorTop;
-
-                //wait so we get the appearance of typing
-                Thread.Sleep(writeDelay); //would be sick to change the pause timer depnding on if we're at the end of a sentence or not. Check for punctuation.
-
-                //clear the lines we just wrote to so we can write the updated text next pass
-                for(int j = startLine; j < endLine; j++)
-                {
-                    Console.SetCursorPosition(0, j);
-                    Console.Write(new string(' ', Console.WindowWidth));
-                }
-                //put our cursor back where we started, so we can print displayText on the correct row next pass
-                Console.SetCursorPosition(0, startLine);
-
-                //if we get user input while reading the text, skip to the end
-                if (Console.KeyAvailable)
-                {
-                    displayText = nodeText;
-                    break;
-                }
-
-            }
-            //print the final version of displayText, which should now be the same as nodeText
-            Console.WriteLine($"{displayText}");
-            isWriting = false;
-
-            return $"{node.Text.TextSpeaker}{displayText}";
-        }
+            //if input is good go to the next node
+            MoveToNode(node.NextNode);
+        } 
     }
 }
---
+++ Dialogue System Solution/DialogueLibrary/DialoguePrinter.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DialogueLibrary
+{
+    public static class DialoguePrinter
+    {
+        private const int LetterWriteDelay = 50;
+        private const int CommaWriteDelay = 100;
+        private const int PunctuationWriteDelay = 200;
+
+        public static bool IsWriting { get; private set; }
+        public static int WriteDelay { get; private set; } = LetterWriteDelay;
+        public static string TextToPrint { get; private set; } = "";
+
+        public static void PrintText(DialogueNode node, bool printOneAtATime)
+        {
+            IsWriting = true;
+            TextToPrint = "";
+
+            Console.WriteLine(node.Text.TextSpeaker);
+            string nodeText = $"{node}";
+
+            if (printOneAtATime)
+            {
+                PrintOneCharAtATime(nodeText);
+            }
+            else
+            {
+                TextToPrint = nodeText;
+            }
+
+            //print the final version of TextToPrint, which should now be the same as nodeText
+            Console.WriteLine($"{TextToPrint}");
+            IsWriting = false;
+        }
+
+        private static void PrintOneCharAtATime(string nodeText)
+        {
+            for (int i = 0; i < nodeText.Length; i++)
+            {
+                //store the current chunk of the full dialogue text
+                TextToPrint = nodeText.Substring(0, i + 1);
+
+                //determine delay based on what current character we're adding
+                char lastChar = TextToPrint[TextToPrint.Length - 1];
+                DetermineDelay(lastChar);
+
+                //store what line the dialogue starts on
+                int startLine = Console.CursorTop;
+
+                //print the current displayText
+                Console.Write($"{TextToPrint}");
+
+                //store what line the dialogue ends on
+                int endLine = Console.CursorTop;
+
+                //wait so we get the appearance of typing
+                Thread.Sleep(WriteDelay);
+
+                ClearConsoleLines(startLine, endLine);
+
+                //if we get user input while reading the text, skip to the end
+                if (Console.KeyAvailable)
+                {
+                    TextToPrint = nodeText;
+                    break;
+                }
+            }
+        }
+
+        private static void ClearConsoleLines(int startLine, int endLine)
+        {
+            for (int i = startLine; i < endLine; i++)
+            {
+                Console.SetCursorPosition(0, i);
+                Console.Write(new string(' ', Console.WindowWidth));
+            }
+            Console.SetCursorPosition(0, startLine);
+        }
+
+        private static void DetermineDelay(char lastChar)
+        {
+            if (Char.IsPunctuation(lastChar) && lastChar != '\'' && lastChar != '\"')
+            {
+                if (lastChar == ',')
+                {
+                    WriteDelay = CommaWriteDelay;
+                }
+                else
+                {
+                    WriteDelay = PunctuationWriteDelay;
+                }
+            }
+            else
+            {
+                WriteDelay = LetterWriteDelay;
+            }
+        }
+    }
+}
